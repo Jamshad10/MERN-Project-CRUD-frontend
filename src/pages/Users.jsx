7f55e4888dd7d113db2b8d8944216a6c1b4a96eb
Jamshad <123630640+Jamshad10@users.noreply.github.com>
2023-10-08 15:49:58 +0530

    image change

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -75,7 +75,7 @@ const Users = () => {
                             <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                                 <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
                                     <img className="w-10 h-10 rounded-full"
-                                     src={'http://localhost:8000/images/'+ image}
+                                     src={'http://localhost:8000/images/'+ user.image}
                                      alt="user image"
                                       />
                                     <div className="pl-3">
